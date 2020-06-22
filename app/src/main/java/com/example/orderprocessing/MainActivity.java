89001a2bf6431507a0b3ae2bb06b4c43aeafb22d
kaustubh <kaustubh.khulbe@gmail.com>
2020-06-22 11:29:51 -0700

    started work on reading data

--- app/src/main/java/com/example/orderprocessing/MainActivity.java
+++ app/src/main/java/com/example/orderprocessing/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initializeInputFields();
         initalizeFirebaseFields();
+
     }
 
     private void initalizeFirebaseFields() {
--- app/src/main/java/com/example/orderprocessing/SuccessfulyLogedIn.java
+++ app/src/main/java/com/example/orderprocessing/SuccessfulyLogedIn.java
@@ -3,12 +3,41 @@ package com.example.orderprocessing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class SuccessfulyLogedIn extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successfuly_loged_in);
+
+        //TODO finish reading data
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                User user = dataSnapshot.getValue(User.class);
+                System.out.println(user.isCustomer + "    " + user.username);
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+       // mDataBase.addValueEventListener(postListener);
     }
 }
