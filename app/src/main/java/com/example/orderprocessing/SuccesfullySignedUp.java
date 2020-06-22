ae3929eb53d2dfdc49d4b93b22c87e69e83f6fc7
kaustubh <kaustubh.khulbe@gmail.com>
2020-06-21 20:56:11 -0700

    improved sign up

--- app/src/main/java/com/example/orderprocessing/SuccesfullySignedUp.java
+++ app/src/main/java/com/example/orderprocessing/SuccesfullySignedUp.java
@@ -2,13 +2,30 @@ package com.example.orderprocessing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccesfullySignedUp extends AppCompatActivity {
+
+    Button goToMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succesfully_signed_up);
+        setUpButton();
+    }
+
+    private void setUpButton() {
+        goToMenu = (Button) findViewById(R.id.gotToMenu);
+        goToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SuccesfullySignedUp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
